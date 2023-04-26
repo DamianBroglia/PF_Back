4c83698e9cee1ef6de5457dcdc783709e9261bce
Elias Yanzon <eliasmotog3@gmail.com>
2023-04-26 10:44:56 -0300

    Destructuring of models in db to later use them in the tables

--- src/db.js
+++ src/db.js
@@ -28,6 +28,9 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
+const {Activity, Comment, Hotel, Package, Reservation, Restaurant, User, Userinfo} = sequelize.models
+
+
 
 
 module.exports = {
